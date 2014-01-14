a1c07eb1c3b7c8fbd1d6e7a7242e63003a04e757
Lucas Galego and Vic Ramon <dev+lrgalego+vicramon@hashrocket.com>
2014-01-14 14:47:55 -0500

    Rename assert_remote_file_contents to assert_remote_file

--- spec/helpers.rb
+++ spec/helpers.rb
@@ -2,7 +2,7 @@ require 'open-uri'
 
 module Helpers
 
-  def assert_remote_file_contents(file, url)
+  def assert_remote_file(file, url)
     open("tmp/#{file}").each_byte.to_a.should == open(url).each_byte.to_a
   end
 
--- spec/lib/spicerack_spec.rb
+++ spec/lib/spicerack_spec.rb
@@ -9,10 +9,14 @@ describe 'spicerack' do
 
   let(:task_path) { "tasks/spicerack" }
 
-  before(:all) do
+  before :all  do
     ENV["GEM_TESTING"] = 'true'
     Rake.application.rake_require(task_path)
     Rake::Task.define_task(:environment)
+  end
+
+  before :each do
+    Rake.application.invoke_task "spicerack:#{subject}"
   end
 
   after(:all) do
@@ -20,29 +24,21 @@ describe 'spicerack' do
   end
 
   describe 'cask' do
-    before do
-      Rake.application.invoke_task "spicerack:cask"
-    end
-
     it "creates sass files from remote sources" do
-      assert_remote_file_contents "lib/assets/stylesheets/_reset.sass",
+      assert_remote_file "lib/assets/stylesheets/_reset.sass",
         "https://gist.github.com/camerond/7681450/raw/whitespace-reset.sass"
-      assert_remote_file_contents "lib/assets/stylesheets/cask.sass",
+      assert_remote_file "lib/assets/stylesheets/cask.sass",
         "https://gist.github.com/camerond/8030122/raw/cask.sass"
     end
   end
 
   describe 'minical' do
-    before do
-      Rake.application.invoke_task "spicerack:minical"
-    end
-
-    it "creates coffee, sass and png files from remote sources" do
-      assert_remote_file_contents 'lib/assets/javascripts/jquery.minical.js.coffee',
+    it "creates coffeescript, sass and png files from remote sources" do
+      assert_remote_file 'lib/assets/javascripts/jquery.minical.js.coffee',
         'https://raw.github.com/camerond/jquery-minical/master/source/javascripts/jquery.minical.js.coffee'
-      assert_remote_file_contents 'lib/assets/stylesheets/jquery.minical.css.sass',
+      assert_remote_file 'lib/assets/stylesheets/jquery.minical.css.sass',
         'https://raw.github.com/camerond/jquery-minical/master/source/stylesheets/jquery.minical.css.sass'
-      assert_remote_file_contents 'lib/assets/images/jquery_minical_icons.png',
+      assert_remote_file 'lib/assets/images/jquery_minical_icons.png',
         'https://raw.github.com/camerond/jquery-minical/master/source/images/jquery_minical_icons.png'
     end
   end
